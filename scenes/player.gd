adba1ddff7ddd5e01b057f79fb3ac903a8578959
Littletailsdev <littletailsinlife@gmail.com>
2024-11-03 11:57:50 +0100

    different layers and occlusion
    trying out how to make walls invisible

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var tile_map_layer = $"../TileMapLayer"
 
 
 const SPEED = 100.0
@@ -13,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
